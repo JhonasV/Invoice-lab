b45b54b6742a050d00695e577ba6a0e95878b497
JhonasV <jhonas@gmail.com>
2021-08-24 00:31:15 -0400

    feat: index file refactorized with typescript class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import * as express from 'express'
 import { config as dotEnvConfig } from 'dotenv'
 import * as cors from 'cors'
-
 import routers from './routers'
-dotEnvConfig({path: __dirname + '../../.env'})
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
 
-// mouting the routes
-routers(app)
+class Server {
+  app: express.Application
+  constructor(){
+    this.app =  express()
+    routers(this.app)
+    this.middlewares()
+    this.init()
+  }
 
-app.listen(process.env.SERVER_PORT , () => console.log(`Server running on port ${process.env.SERVER_PORT}`))
+  middlewares(){
+    dotEnvConfig({path: __dirname + '../../.env'})
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({extended: true}))
+    this.app.use(cors())
+
+  }
+
+  init(){
+    this.app.listen(process.env.SERVER_PORT , () => console.log(`Server running on port ${process.env.SERVER_PORT}`))
+  }
+}
+
+new Server()
