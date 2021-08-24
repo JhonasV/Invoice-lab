47e5e57c638c743f6f6f1d2f56c19e45ec7a0adb
JhonasV <jhonas@gmail.com>
2021-08-24 00:33:22 -0400

    fix: parameter type fixed

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,6 @@
 import { customerRouter } from "./customers.router"
 import * as express from 'express'
 
-export default (app: express.Applicationn) => {
+export default (app: express.Application) => {
   app.use('/api/v1/customers', customerRouter)
 }
